22967f6ddd176fff5bfd778ccb0488c3632d28f1
Adam Gausmann <agausmann@fastmail.com>
2021-07-27 00:27:39 -0500

    Update to 1.17.1, and update support for Sodium 0.3.0

--- src/main/java/dev/gaussian/signsearcher/mixin/sodium/SodiumWorldRendererMixin.java
+++ src/main/java/dev/gaussian/signsearcher/mixin/sodium/SodiumWorldRendererMixin.java
@@ -17,30 +17,30 @@ import java.util.SortedSet;
 
 @Mixin(SodiumWorldRenderer.class)
 public abstract class SodiumWorldRendererMixin {
-//    @Redirect(
-//        //public void renderTileEntities(MatrixStack matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions,
-//        //                             Camera camera, float tickDelta) {
-//        method = "renderTileEntities",
-//
-//        //BlockEntityRenderDispatcher.INSTANCE.render(blockEntity, tickDelta, matrices, consumer);
-//        at = @At(
-//            value = "INVOKE",
-//            target = "net/minecraft/client/render/block/entity/BlockEntityRenderDispatcher.render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V",
-//            ordinal = 0
-//        )
-//    )
-//    public void renderWithOutline(BlockEntityRenderDispatcher dispatcher, BlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vcp, MatrixStack _matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> _blockBreakingProgressions, Camera _camera, float _tickDelta) {
-//        if (((BlockEntityExt) blockEntity).isGlowing()) {
-//            OutlineVertexConsumerProvider outlineVcp = bufferBuilders.getOutlineVertexConsumers();
-//            int color = ((BlockEntityExt) blockEntity).getGlowColor();
-//            outlineVcp.setColor(
-//                (color >> 16) & 0xff,
-//                (color >> 8) & 0xff,
-//                color & 0xff,
-//                0xff
-//            );
-//            vcp = outlineVcp;
-//        }
-//        dispatcher.render(blockEntity, tickDelta, matrices, vcp);
-//    }
+    @Redirect(
+        //public void renderTileEntities(MatrixStack matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions,
+        //                             Camera camera, float tickDelta) {
+        method = "renderTileEntities",
+
+        //BlockEntityRenderDispatcher.INSTANCE.render(blockEntity, tickDelta, matrices, consumer);
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V",
+            ordinal = 0
+        )
+    )
+    public void renderWithOutline(BlockEntityRenderDispatcher dispatcher, BlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vcp, MatrixStack _matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> _blockBreakingProgressions, Camera _camera, float _tickDelta) {
+        if (((BlockEntityExt) blockEntity).isGlowing()) {
+            OutlineVertexConsumerProvider outlineVcp = bufferBuilders.getOutlineVertexConsumers();
+            int color = ((BlockEntityExt) blockEntity).getGlowColor();
+            outlineVcp.setColor(
+                (color >> 16) & 0xff,
+                (color >> 8) & 0xff,
+                color & 0xff,
+                0xff
+            );
+            vcp = outlineVcp;
+        }
+        dispatcher.render(blockEntity, tickDelta, matrices, vcp);
+    }
 }
